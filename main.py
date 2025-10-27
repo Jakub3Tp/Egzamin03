9982a0327a730c8c367fa6e7bec3e4ae3c30d272
5TpNowickaMonika <egzamin@poczta.pl>
2025-10-27 08:36:44 +0100

    27.10.2025

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-def print_hi(koscy: int):
-    print(f'Hi, {koscy}')
+import random
+
+
+def rzut_koscmi(ile_koscy):
+    wyniki = [random.randint(1, 6) for _ in range(ile_koscy)]
+    return wyniki
+
+def punkty(wyniki):
+    punkty = 0
+    for punkt in set(wyniki):
+        ile_razy = wyniki.count(punkt)
+        if ile_razy >= 2:
+            punkty += punkt * ile_razy
+    return punkty
+
+def gra():
+    while True:
+        liczba_kosci = int(input("Podaj liczbę kostek do rzucenia (3-10): \n"))
+        if 3 <= liczba_kosci <= 10:
+            break
+    while True:
+        wyniki = rzut_koscmi(liczba_kosci)
+        for i, wynik in enumerate(wyniki, start=1):
+            print(f"Kostka {i}, {wynik}")
+        punkciki = punkty(wyniki)
+        print(f"Uzyskano: {punkciki} punktów. \n")
+        while True:
+            jeszcze_raz = input("Czy chcesz zagrać ponownie? (t/n): ").lower()
+            if jeszcze_raz in ("t", "n"):
+                break
+        if jeszcze_raz == "n":
+            break
 
 
 if __name__ == '__main__':
-    n_koscy = int(input("Ile kostek chcesz wyrzucić (3-10): "))
-
+    gra()
